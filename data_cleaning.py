fb812f728538e43f7c357153027c13911804ec48
ngocmphan <ngocm.phan@gmail.com>
2023-05-04 16:17:35 -0400

    Assess list of countries

--- data_cleaning.py
+++ data_cleaning.py
@@ -10,3 +10,31 @@ df_unemployment_rate = pd.read_excel("imf-dm-export-20230502.xls")
 
 # Preliminary variable selection and main data frame creation
 # Ensuring all countries are available for joint
+
+
+def data_frame_check(df):
+    print(df.info())
+    print(df.head(5))
+
+
+data_frame_check(df_birth_rate)
+data_frame_check(df_gdp)
+data_frame_check(df_happiness_index)
+data_frame_check(df_unemployment_rate)
+
+country_birth_rate = len(df_birth_rate["country"].unique())
+
+country_gdp = len(df_gdp["country"].unique())
+
+country_happiness_index = len(df_happiness_index["Country name"].unique())
+
+country_unemployment_rate = len(
+    df_unemployment_rate["Unemployment rate (Percent)"].unique()
+)
+
+print(
+    country_birth_rate, country_gdp, country_happiness_index, country_unemployment_rate
+)
+
+# Note that the countries lists between the datasets are not the same:
+# Require identification of countries in scope for all datasets
